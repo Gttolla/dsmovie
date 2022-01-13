3c2c1c273049d7aee4e775217c343034084aa6fe
Gttolla <lucaspedrosupergeeks@gmail.com>
2022-01-13 18:52:37 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,23 @@
-import Navbar from "./components/Navbar";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Listing from 'components/pages/Listing';
+import Form from 'components/pages/Form';
+import Navbar from "components/Navbar";
 
 function App() {
-  return(
-    <Navbar/>
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
