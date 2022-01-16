ed75f274dd0407608a3bb06311ec6b99fd044e6a
Gttolla <lucaspedrosupergeeks@gmail.com>
2022-01-16 19:24:29 -0300

    useState, UseEffect

--- frontend/src/components/pages/Listing/index.tsx
+++ frontend/src/components/pages/Listing/index.tsx
@@ -3,13 +3,22 @@ import MovieCard from "components/MovieCard";
 import axios from "axios";
 import './styles.css'
 import { BASE_URL } from "utils/requests";
+import { useState } from "react";
+import { useEffect } from "react";
+import { MoviePage } from "types/movie";
 
 function Listing() {
 
-    axios.get(`${BASE_URL}/movies?size=12&page=0`)
-    .then(response => {
-        console.log(response.data);
-    })
+    const [pageNumber, setPageNumber] = useState(0);
+        
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=0`)
+        .then(response => {
+            const data = response.data as MoviePage;
+            console.log(data);
+            setPageNumber(data.number);
+        })
+    }, [])
 
     return (
         <>
@@ -35,7 +44,7 @@ function Listing() {
                     <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                         <MovieCard />
                     </div>
-            
+
                 </div>
             </div>
 
